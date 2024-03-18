8c12370e2e7771297896e378ca752b5119ae00d0
ambosing <ambosing_@naver.com>
2024-03-18 14:50:18 +0900

    Feat: 초기 프로젝트 세팅

---
+++ src/test/java/com/woowa/MealbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
