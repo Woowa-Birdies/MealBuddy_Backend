dd591a0825517096412754ccbfb128133a509fca
ambosing <ambosing_@naver.com>
2024-03-31 12:43:13 +0900

    feat. Cors 설정 추가

---
+++ src/main/java/com/woowa/common/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.woowa.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Value("${cors.allowedOriginPatterns")
+	private String corsAllowOriginPatterns;
+
+	@Value("${cors.allowedMethods}")
+	private String allowedMethods;
+
+	@Value("${cors.allowedHeaders}")
+	private String allowedHeaders;
+
+	@Value("${cors.exposedHeaders}")
+	private String exposedHeaders;
+
+	@Value("${cors.maxAge}")
+	private Long maxAge;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns(corsAllowOriginPatterns)
+			.allowedMethods(allowedMethods)
+			.allowedHeaders(allowedHeaders)
+			.exposedHeaders(exposedHeaders)
+			.maxAge(maxAge);
+	}
+}
