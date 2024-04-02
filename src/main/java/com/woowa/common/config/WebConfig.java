32370921cb3c6d2550372fed32b37e0dce4cbbb3
ambosing <ambosing_@naver.com>
2024-04-02 18:54:41 +0900

    Fix: Cors Config 변수 매핑 수정

--- src/main/java/com/woowa/common/config/WebConfig.java
+++ src/main/java/com/woowa/common/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Value("${cors.allowedOriginPatterns")
+	@Value("${cors.allowedOriginPatterns}")
 	private String corsAllowOriginPatterns;
 
 	@Value("${cors.allowedMethods}")
