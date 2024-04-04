ad81cfeb6e8abac713a7a3f420b8f5148eab8135
Jhsysng <ysng1118@naver.com>
2024-04-05 00:26:26 +0900

    feat: room, intersection room_user entity생성

---
+++ src/main/java/com/woowa/room/domain/Room.java
@@ -0,0 +1,23 @@
+package com.woowa.room.domain;
+
+import com.woowa.common.domain.BaseEntity;
+import com.woowa.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Room extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    private String roomName;
+}
