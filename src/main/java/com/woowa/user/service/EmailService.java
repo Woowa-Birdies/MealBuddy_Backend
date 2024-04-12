adbccead2297e0fb4ae753f23d4198cfb3a90f57
ambosing <ambosing_@naver.com>
2024-04-12 21:40:10 +0900

    Feat: 이메일 인증 확인 기능 구현

--- src/main/java/com/woowa/user/service/EmailService.java
+++ src/main/java/com/woowa/user/service/EmailService.java
@@ -1,13 +1,17 @@
 package com.woowa.user.service;
 
 import java.io.UnsupportedEncodingException;
+import java.time.Instant;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.woowa.common.domain.DuplicateException;
 import com.woowa.common.domain.EmailException;
+import com.woowa.common.domain.NotAuthorizedException;
+import com.woowa.user.domain.EmailVerification;
 import com.woowa.user.repository.EmailRepository;
+import com.woowa.user.service.dto.EmailVerificationDTO;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
@@ -25,20 +29,29 @@ public class EmailService {
 		emailRepository.findByUserId(userId).ifPresent((item) -> {
 			throw new DuplicateException(userId, "EmailVerification");
 		});
+		String token = tokenGenerator.generateEmailVerificationToken();
 
 		try {
-			javaMailSender.send(createMessage(toEmail));
+			javaMailSender.send(createVerificationMessage(toEmail, token));
+			emailRepository.save(new EmailVerification(token, Instant.now(), userId));
 		} catch (Exception e) {
 			throw new EmailException();
 		}
 	}
 
-	private MimeMessage createMessage(String toEmail) throws MessagingException, UnsupportedEncodingException {
+	public void verifyEmailToken(EmailVerificationDTO emailVerificationDTO) {
+		EmailVerification emailVerification = emailRepository.findByToken(emailVerificationDTO.getToken())
+			.orElseThrow(() -> new NotAuthorizedException("이메일 인증에 실패하였습니다."));
+		emailVerification.checkExpired();
+	}
+
+	private MimeMessage createVerificationMessage(String toEmail, String token) throws
+		MessagingException,
+		UnsupportedEncodingException {
 		MimeMessage message = javaMailSender.createMimeMessage();
 
 		message.addRecipients(MimeMessage.RecipientType.TO, toEmail);
 		message.setSubject("[냠메이트] 인증 메일입니다.");
-		String token = tokenGenerator.generateEmailVerificationToken();
 
 		String content = String.format("""
 			냠메이트 인증 메일입니다.
