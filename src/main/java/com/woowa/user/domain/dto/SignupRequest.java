074ce7940910da23e6f67402f365128f1dd5751d
ambosing <ambosing_@naver.com>
2024-04-14 00:33:15 +0900

    Feat: 유저 추가 정보 기입 테스트 추가

--- src/main/java/com/woowa/user/domain/dto/SignupRequest.java
+++ src/main/java/com/woowa/user/domain/dto/SignupRequest.java
@@ -6,6 +6,7 @@ import com.woowa.user.domain.Gender;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class SignupRequest {
+	@NotNull
+	private Long userId;
 	@NotEmpty
 	private String nickname;
 	@Past
@@ -20,4 +23,12 @@ public class SignupRequest {
 	private Gender gender;
 	@Email
 	private String email;
+
+	public SignupRequest(Long userId, String nickname, LocalDateTime birthDate, Gender gender, String email) {
+		this.userId = userId;
+		this.nickname = nickname;
+		this.birthDate = birthDate;
+		this.gender = gender;
+		this.email = email;
+	}
 }
--- src/main/java/com/woowa/user/service/UserService.java
+++ src/main/java/com/woowa/user/service/UserService.java
@@ -1,9 +1,12 @@
 package com.woowa.user.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.woowa.common.domain.DuplicateException;
+import com.woowa.common.domain.ResourceNotFoundException;
 import com.woowa.user.domain.User;
 import com.woowa.user.domain.dto.SignupRequest;
 import com.woowa.user.repository.UserRepository;
@@ -15,19 +18,23 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 	private final UserRepository userRepository;
 
-	public void checkDuplicateNickname(String nickName) {
-		userRepository.findByNickname(nickName).ifPresent((item) -> {
-			throw new DuplicateException(nickName, "User");
-		});
+	public Optional<User> findByNickname(String nickName) {
+		return userRepository.findByNickname(nickName);
 	}
 
 	@Transactional
 	public Long setAdditionalInfo(SignupRequest signupRequest) {
-		checkDuplicateNickname(signupRequest.getNickname());
-		User user = userRepository.findByEmail(signupRequest.getEmail())
-			.orElseThrow(() -> new DuplicateException(signupRequest.getEmail(), "User"));
+		User user = getByUserId(signupRequest.getUserId());
+		userRepository.findByEmail(signupRequest.getEmail()).ifPresent(anotherUser -> {
+			throw new DuplicateException(signupRequest.getEmail(), "User");
+		});
 
 		user.updateAdditionalInfo(signupRequest);
+
 		return user.getId();
+	}
+
+	private User getByUserId(Long userId) {
+		return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(userId, "User"));
 	}
 }
