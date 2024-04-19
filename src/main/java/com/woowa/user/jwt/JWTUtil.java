5fff286bcea9c491c7fd5dd566977b2e393d4a71
ambosing <ambosing_@naver.com>
2024-04-19 16:18:32 +0900

    Feat: Refresh 재발급 테스트 추가

--- src/main/java/com/woowa/user/jwt/JWTUtil.java
+++ src/main/java/com/woowa/user/jwt/JWTUtil.java
@@ -63,7 +63,7 @@ public class JWTUtil {
 			.claim("userId", userId)
 			.claim("role", role)
 			.issuedAt(new Date(System.currentTimeMillis()))
-			.expiration(new Date(System.currentTimeMillis() + expiredMs))
+			.expiration(new Date(System.currentTimeMillis() + expiredMs * 1000))
 			.signWith(secretKey)
 			.compact();
 	}
--- src/test/java/com/woowa/user/controller/AuthControllerTest.java
+++ src/test/java/com/woowa/user/controller/AuthControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import com.woowa.IntegrationTestSupport;
 import com.woowa.common.domain.ResourceNotFoundException;
 import com.woowa.user.domain.SocialLogin;
+import com.woowa.user.jwt.JWTUtil;
 import com.woowa.user.repository.SocialLoginRepository;
 
 import jakarta.servlet.http.Cookie;
@@ -21,6 +22,9 @@ class AuthControllerTest extends IntegrationTestSupport {
 
 	@Autowired
 	private SocialLoginRepository socialLoginRepository;
+
+	@Autowired
+	private JWTUtil jwtUtil;
 
 	@Test
 	@DisplayName("사용자는 로그아웃을 할 수 있다")
@@ -49,5 +53,32 @@ class AuthControllerTest extends IntegrationTestSupport {
 		Assertions.assertThatThrownBy(() -> socialLoginRepository.findById(savedSocialLogin.getId())
 				.orElseThrow(() -> new ResourceNotFoundException(socialLogin.getId(), "SocialLogin")))
 			.isInstanceOf(ResourceNotFoundException.class);
+	}
+
+	@Test
+	@DisplayName("사용자는 액세스 토큰과 리프레시 토큰을 갱신할 수 있다")
+	@WithMockUser
+	void 사용자는_액세스_토큰과_리프레시_토큰을_갱신할_수_있다() throws Exception {
+		//given
+		SocialLogin socialLogin = new SocialLogin(1L, "KAKAO", "asdsadsad");
+		String refreshToken = jwtUtil.createJwt(REFRESH_TOKEN, socialLogin.getUserId(), ROLE_USER,
+			REFRESH_TOKEN_DURATION);
+		socialLogin.update(refreshToken);
+		SocialLogin savedSocialLogin = socialLoginRepository.save(socialLogin);
+		System.out.println("refreshToken test : " + savedSocialLogin.getRefreshToken().equals(refreshToken));
+		System.out.println("savedSocialLogin.getId() = " + savedSocialLogin.getId());
+
+		Cookie cookie = new Cookie(REFRESH_TOKEN, refreshToken);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(true);
+		cookie.setPath("/");
+		cookie.setMaxAge(Math.toIntExact(REFRESH_TOKEN_DURATION));
+		//when
+		//then
+		mockMvc.perform(post("/api/reissue")
+				.cookie(cookie))
+			.andExpect(status().isOk());
+		SocialLogin updateSocialLogin = socialLoginRepository.findById(savedSocialLogin.getId()).get();
+		Assertions.assertThat(updateSocialLogin.getRefreshToken()).isNotEqualTo(refreshToken);
 	}
 }
