23955ec862b64547252dc6ad9c6a31afaec9001b
Jhsysng <ysng1118@naver.com>
2024-04-19 15:04:03 +0900

    feat: ws 예외

--- src/main/java/com/woowa/room/repository/RoomUserRepository.java
+++ src/main/java/com/woowa/room/repository/RoomUserRepository.java
@@ -3,7 +3,10 @@ package com.woowa.room.repository;
 import com.woowa.room.domain.RoomUser;
 import com.woowa.room.repository.support.RoomUserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomUserRepository extends JpaRepository<RoomUser, Long>, RoomUserRepositoryCustom {
