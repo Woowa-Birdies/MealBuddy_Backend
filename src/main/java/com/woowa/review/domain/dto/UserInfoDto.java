27e1d21952887f7ae7ae5e9f2240114bf4608aa3
Han <inte098@naver.com>
2024-04-19 17:16:29 +0900

    feat: 후기 작성 위한 유저 정보 api

---
+++ src/main/java/com/woowa/review/domain/dto/UserInfoDto.java
@@ -0,0 +1,35 @@
+package com.woowa.review.domain.dto;
+
+import com.woowa.user.domain.Gender;
+import com.woowa.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserInfoDto {
+    private Long userId;
+    private String nickname;
+    private String introduce;
+    private LocalDateTime birthDate;
+    private Gender gender;
+    private String email;
+    private String image;
+
+    public static UserInfoDto fromUser(User user) {
+        return new UserInfoDto(
+                user.getId(),
+                user.getNickname(),
+                user.getIntroduce(),
+                user.getBirthDate(),
+                user.getGender(),
+                user.getEmail(),
+                user.getImage()
+        );
+    }
+}
