79e875c49b87ecef6dbe3637331511ef7626151e
Han <inte098@naver.com>
2024-04-19 15:21:30 +0900

    fix: 유저 리스트 반환 메서드 수정

--- src/main/java/com/woowa/room/repository/RoomUserRepository.java
+++ src/main/java/com/woowa/room/repository/RoomUserRepository.java
@@ -5,6 +5,9 @@ import com.woowa.room.repository.support.RoomUserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomUserRepository extends JpaRepository<RoomUser, Long>, RoomUserRepositoryCustom {
+    List<RoomUser> findByRoomId(Long roomId);
 }
