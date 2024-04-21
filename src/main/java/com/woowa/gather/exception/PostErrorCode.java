c8befdffe3f06963bc9668acd3478a1498573201
Jiyun Yun <jeeyunyun8@gmail.com>
2024-04-22 02:54:31 +0900

    fix: USER_NOT_VERIFICATION 에러 BAD_REQUEST(400)으로 변경

--- src/main/java/com/woowa/gather/exception/PostErrorCode.java
+++ src/main/java/com/woowa/gather/exception/PostErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum PostErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"PostEx001", "사용자를 찾을 수 없습니다."),
-    USER_NOT_VERIFICATION(HttpStatus.NOT_FOUND,"PostEx002", "사용자가 추가 인증을 하지 않았습니다."),
+    USER_NOT_VERIFICATION(HttpStatus.BAD_REQUEST,"PostEx002", "사용자가 추가 인증을 하지 않았습니다."),
     ;
 
     private HttpStatus httpStatus;
