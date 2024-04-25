efd5fc2ceef471ad3b553909b8c8b215c65651cb
Jhsysng <ysng1118@naver.com>
2024-04-25 22:05:08 +0900

    feat: response postId추가

--- src/main/java/com/woowa/room/service/RoomService.java
+++ src/main/java/com/woowa/room/service/RoomService.java
@@ -69,6 +69,7 @@ public class RoomService {
         return RoomResponseDto.builder()
                 .roomId(createdRoom.getId())
                 .roomName(createdRoom.getRoomName())
+                .postId(postId)
                 .build();
     }
 
@@ -108,6 +109,7 @@ public class RoomService {
         return RoomResponseDto.builder()
                 .roomId(savedRoomUser.getRoom().getId())
                 .roomName(savedRoomUser.getRoom().getRoomName())
+                .postId(postId)
                 .build();
     }
 
